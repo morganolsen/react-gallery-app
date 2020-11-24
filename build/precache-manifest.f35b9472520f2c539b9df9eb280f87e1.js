baf14465bf735e2b1189475cfa8ba3f142e2a95a
Morgan Olsen <git@morganolsen.no>
2020-11-24 14:13:21 +0100

    add build for github pages

---
+++ build/precache-manifest.f35b9472520f2c539b9df9eb280f87e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d471105d7e756a20f4705757a0c6773c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25df0801d48a0dea7453",
+    "url": "/static/css/main.a6689c7b.chunk.css"
+  },
+  {
+    "revision": "19e2062bfb5b556815e1",
+    "url": "/static/js/2.c20de75d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c20de75d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25df0801d48a0dea7453",
+    "url": "/static/js/main.537010ea.chunk.js"
+  },
+  {
+    "revision": "091c55ec2b56a264ad18",
+    "url": "/static/js/runtime-main.fb16122e.js"
+  }
+]);
